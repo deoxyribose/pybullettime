d063275211e6b75a7790ac2d9920a91166e48edc
Søren Enggaard Hansen <seh@sehansen.dk>
2024-03-03 18:36:17 +0100

    Turned the shock absorber into an object

--- s_shock_absorber.py
+++ s_shock_absorber.py
@@ -11,6 +11,36 @@ rng = np.random.default_rng()
 springConstant = 2 ** 6
 
 
+class ShockAbsorber:
+    def __init__(self, springConstant, dampingForce):
+        self.springConstant = springConstant
+        self.dampingForce = dampingForce
+        self.registered = False
+
+    def register(self, shockId, linkParent, linkChild, linkAxis):
+        self.shockId = shockId
+        self.linkParent = linkParent
+        self.linkChild = linkChild
+        self.linkAxis = linkAxis
+        pb.setJointMotorControl2(self.shockId,
+                                 self.linkChild,
+                                 pb.VELOCITY_CONTROL,
+                                 targetVelocity=0,
+                                 force=self.dampingForce)
+        self.registered = True
+
+    def update(self):
+        if not self.registered:
+            raise RuntimeError("You need to register before you can simulate")
+
+        shockState = pb.getJointState(self.shockId, self.linkChild)
+        springForce = -self.springConstant * shockState[0]
+
+        pb.applyExternalForce(self.shockId, self.linkParent,
+                              [-springForce * x for x in self.linkAxis], [0, 0, 0], pb.LINK_FRAME)
+        pb.applyExternalForce(self.shockId, self.linkChild,
+                              [springForce * x for x in self.linkAxis], [0, 0, 0], pb.LINK_FRAME)
+
 def main():
     physicsClient = pb.connect(pb.GUI)
     pb.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -29,23 +59,18 @@ def main():
                                  linkJointTypes=[pb.JOINT_PRISMATIC],
                                  linkJointAxis=[[0, 0, 1]])
 
-    pb.setJointMotorControl2(shockId, 0, pb.VELOCITY_CONTROL, targetVelocity=0, force=2)
-    # pb.setJointMotorControl2(shockId, 0, pb.VELOCITY_CONTROL, targetVelocity=0, force=8.95)
-
     sphereCol = pb.createCollisionShape(pb.GEOM_SPHERE)
     spereId = pb.createMultiBody(baseMass=1.25, basePosition=[0, 0, 5], baseCollisionShapeIndex=sphereCol, baseVisualShapeIndex=-1)
 
+    sp = ShockAbsorber(springConstant, 2)
+    sp.register(shockId, -1, 0, [0, 0, 1])
 
     while True:
         pb.stepSimulation()
         time.sleep(1.0/240.0)
 
-        shockState = pb.getJointState(shockId, 0)
-        springForce = -springConstant * shockState[0]
-        pb.applyExternalForce(shockId, 0, [0, 0, springForce], [0, 0, 0], pb.LINK_FRAME)
-        pb.applyExternalForce(shockId, -1, [0, 0, -springForce], [0, 0, 0], pb.LINK_FRAME)
         # update shock joint forces
-
+        sp.update()
 
 if __name__ == "__main__":
     main()
