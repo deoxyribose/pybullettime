a3065631c304ab10f2546f8148c731fc01456a9a
frans <frans.zdyb@deondigital.com>
2024-06-09 15:12:51 +0200

    move relative to camera, not world

--- keyboard_control.py
+++ keyboard_control.py
@@ -1,31 +1,40 @@
 import pybullet as pb
+import numpy as np
 
 def move_arrows():
+    # moves the camera along the x and y axis of the world
     keys = pb.getKeyboardEvents()
     cam = pb.getDebugVisualizerCamera()
-    print(keys)
-    #Keys to change camera
+
+    
+
+    x_delta = 0
+    y_delta = 0
+    
     if keys.get(65298):  # DOWN
-        xyz = cam[11]
-        x= float(xyz[0]) + 0.125
-        y = xyz[1]
-        z = xyz[2]
-        pb.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
+        y_delta = - 0.125
     if keys.get(65297):   # UP
-        xyz = cam[11]
-        x= float(xyz[0]) - 0.125
-        y = xyz[1]
-        z = xyz[2]
-        pb.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
+        y_delta = 0.125
     if keys.get(65296):   # LEFT
-        xyz = cam[11]
-        x = xyz[0] 
-        y = float(xyz[1]) + 0.125
-        z = xyz[2]
-        pb.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
+        x_delta = 0.125
     if keys.get(65295):  # RIGHT
-        xyz = cam[11]
-        x = xyz[0] 
-        y = float(xyz[1]) - 0.125
-        z = xyz[2]
-        pb.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z]) 
+        x_delta = - 0.125
+    camera_delta = [x_delta, y_delta, 0]
+    new_world_coords = transform_coordinates(camera_delta, cam)
+    pb.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=new_world_coords) 
+
+def transform_coordinates(camera_delta, cam):
+    # rotates the coordinates from the camera to the world
+    world_coords = cam[11]
+
+    viewMatrix = cam[2]
+    viewMatrix = np.array(viewMatrix).reshape(4, 4)
+
+    rotMatrix = viewMatrix[:3, :3]
+    translation = viewMatrix[:3, 3]
+
+    camera_delta = np.array(camera_delta) - translation
+    world_delta = rotMatrix @ camera_delta
+
+    new_coords = world_coords + world_delta
+    return new_coords
